4fecdf17d1def80b349bd84e58ec042f997fcdcf
Ben Darnell <ben@bendarnell.com>
2016-05-22 13:12:40 -0400

    Use run_transaction for all DB operations

--- flask-sqlalchemy/hello.py
+++ flask-sqlalchemy/hello.py
@@ -2,11 +2,14 @@ from datetime import datetime
 from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy.orm
+from cockroachdb.sqlalchemy import run_transaction
 
 
 app = Flask(__name__)
 app.config.from_pyfile('hello.cfg')
 db = SQLAlchemy(app)
+sessionmaker = sqlalchemy.orm.sessionmaker(db.engine)
 
 
 class Todo(db.Model):
@@ -26,9 +29,11 @@ class Todo(db.Model):
 
 @app.route('/')
 def show_all():
-    return render_template('show_all.html',
-        todos=Todo.query.order_by(Todo.pub_date.desc()).all()
-    )
+    def callback(session):
+        return render_template(
+            'show_all.html',
+            todos=session.query(Todo).order_by(Todo.pub_date.desc()).all())
+    return run_transaction(sessionmaker, callback)
 
 
 @app.route('/new', methods=['GET', 'POST'])
@@ -39,9 +44,10 @@ def new():
         elif not request.form['text']:
             flash('Text is required', 'error')
         else:
-            todo = Todo(request.form['title'], request.form['text'])
-            db.session.add(todo)
-            db.session.commit()
+            def callback(session):
+                todo = Todo(request.form['title'], request.form['text'])
+                session.add(todo)
+            run_transaction(sessionmaker, callback)
             flash(u'Todo item was successfully created')
             return redirect(url_for('show_all'))
     return render_template('new.html')
@@ -49,10 +55,11 @@ def new():
 
 @app.route('/update', methods=['POST'])
 def update_done():
-    for todo in Todo.query.all():
-        todo.done = ('done.%d' % todo.id) in request.form
+    def callback(session):
+        for todo in session.query(Todo).all():
+            todo.done = ('done.%d' % todo.id) in request.form
+    run_transaction(sessionmaker, callback)
     flash('Updated status')
-    db.session.commit()
     return redirect(url_for('show_all'))
 
 
